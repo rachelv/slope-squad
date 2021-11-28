0a7ccc4e1fd1a3e08c1b14ae27f3002040095adb
Rachel Vecchitto <rachel@magnetbox.org>
2021-11-28 16:51:55 -0700

    customize emails

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,7 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="https://www.slopesquad.com/img/logo/blue.png" class="logo" alt="Slope Squad Logo">
+</a>
+</td>
+</tr>
