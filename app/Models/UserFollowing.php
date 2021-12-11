2397df2fd8241d25f092f890f130cda790fb4fa6
Rachel Vecchitto <rachel@magnetbox.org>
2021-12-11 10:45:21 -0700

    add relationship to get user stats for current season

--- app/Models/UserFollowing.php
+++ app/Models/UserFollowing.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class UserFollowing extends SlopeSquadBaseModel
 {
     use Traits\hasUser;
@@ -15,5 +17,22 @@ class UserFollowing extends SlopeSquadBaseModel
     public function setFollowingUserId(int $followingUserId): void
     {
         $this->following_user_id = $followingUserId;
+    }
+
+    public function currentSeasonFollowerStats(): HasOne
+    {
+        $currentSeasonId = Season::current()->getId();
+
+        return $this->hasOne(StatsUserSeason::class, 'user_id', 'following_user_id')
+            ->ofMany([
+                'id' => 'max',
+            ], function ($query) use ($currentSeasonId) {
+                $query->where('season_id', $currentSeasonId);
+            })->withDefault([
+                'season_id' => $currentSeasonId,
+                'total_snowdays' => 0,
+                'total_mountains' => 0,
+                'total_vertical' => 0,
+            ]);
     }
 }
